cc594bbc07f1a2e28acef68857062e15d905a1fc
kirsch33 <37373320+kirsch33@users.noreply.github.com>
2020-10-31 00:53:38 -0400

    Update module.go

--- module.go
+++ module.go
@@ -94,7 +94,7 @@ var presets = map[string][]string{
 
 func init() {
 	caddy.RegisterModule(realip{})
-	httpcaddyfile.RegisterDirective("realip", parseCaddyfile)
+	httpcaddyfile.RegisterHandlerDirective("realip", parseCaddyfileHandler)
 }
 
 func (module) CaddyModule() caddy.ModuleInfo {
@@ -111,7 +111,7 @@ func (m *module) Provision(ctx caddy.Context) error {
 	return nil
 }
 
-func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+func parseCaddyfileHandler(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
 	var m module
 	err := m.UnmarshalCaddyfile(h.Dispenser)
 	return m, err
@@ -208,49 +208,49 @@ func (m *module) validSource(addr string) bool {
 	return false
 }
 
-func (m *module) ServeHTTP(w http.ResponseWriter, req *http.Request) (int, error) {
+func (m *module) ServeHTTP(w http.ResponseWriter, req *http.Request, handler caddyhttp.Handler) error {
 	host, port, err := net.SplitHostPort(req.RemoteAddr)
-	if err != nil || !m.validSource(host) {
-		if m.Strict {
+	if err != nil || !handler.validSource(host) {
+		if handler.Strict {
 			return 403, fmt.Errorf("Error reading remote addr: %s", req.RemoteAddr)
 		}
-		return m.next.ServeHTTP(w, req) // Change nothing and let next deal with it.
+		return handler.next.ServeHTTP(w, req) // Change nothing and let next deal with it.
 	}
-	if !m.validSource(host) {
-		if m.Strict {
+	if !handler.validSource(host) {
+		if handler.Strict {
 			return 403, fmt.Errorf("Unrecognized proxy ip address: %s", host)
 		}
-		return m.next.ServeHTTP(w, req)
+		return handler.next.ServeHTTP(w, req)
 	}
 
-	if hVal := req.Header.Get(m.Header); hVal != "" {
+	if hVal := req.Header.Get(handler.Header); hVal != "" {
 		//restore original host:port format
 		parts := strings.Split(hVal, ",")
 		for i, part := range parts {
 			parts[i] = strings.TrimSpace(part)
 		}
-		if m.MaxHops != -1 && len(parts) > m.MaxHops {
+		if handler.MaxHops != -1 && len(parts) > handler.MaxHops {
 			return 403, fmt.Errorf("Too many forward addresses")
 		}
 		ip := net.ParseIP(parts[len(parts)-1])
 		if ip == nil {
-			if m.Strict {
+			if handler.Strict {
 				return 403, fmt.Errorf("Unrecognized proxy ip address: %s", parts[len(parts)-1])
 			}
-			return m.next.ServeHTTP(w, req)
+			return handler.next.ServeHTTP(w, req)
 		}
 		req.RemoteAddr = net.JoinHostPort(parts[len(parts)-1], port)
 		for i := len(parts) - 1; i >= 0; i-- {
 			req.RemoteAddr = net.JoinHostPort(parts[i], port)
-			if i > 0 && !m.validSource(parts[i]) {
-				if m.Strict {
+			if i > 0 && !handler.validSource(parts[i]) {
+				if handler.Strict {
 					return 403, fmt.Errorf("Unrecognized proxy ip address: %s", parts[i])
 				}
-				return m.next.ServeHTTP(w, req)
+				return handler.next.ServeHTTP(w, req)
 			}
 		}
 	}
-	return m.next.ServeHTTP(w, req)
+	return handler.next.ServeHTTP(w, req)
 }
 
 func (m *module) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
